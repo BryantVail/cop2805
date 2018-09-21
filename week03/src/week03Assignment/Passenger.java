bcec46a7eb3c7452271c6a3c0f3f5c4ba37762ef
Bryant Vail <Bryanttv@outlook.com>
2018-09-21 09:33:06 -0400

    update toggle alert

--- week03/src/week03Assignment/Passenger.java
+++ week03/src/week03Assignment/Passenger.java
@@ -2,6 +2,21 @@
 
 public class Passenger implements IAlertRaiser{
   
+  private String firstname;
+  private String lastname;
+  private String departureLocation;
+  private Seat seat;
+  
+  public boolean toggleAlert(boolean alertStatus){
+    
+    if(alertStatus == false){
+      return true;
+    }else{
+     return false; 
+    }
+    
+  }//end toggleAlert
+  
   
 }//end class Passenger
 
