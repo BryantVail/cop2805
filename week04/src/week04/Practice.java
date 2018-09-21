7d9cfef8b834f8b568a4fbadb7417a2d0ebd0854
Bryant Vail <Bryanttv@outlook.com>
2018-09-20 20:26:25 -0400

    update

--- week04/src/week04/Practice.java
+++ week04/src/week04/Practice.java
@@ -12,22 +12,49 @@ public class Practice {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		Map capitals = new HashMap<String, String>();
+		Map employees = new HashMap<Integer, Employee>();
 //		Map map2 = new HashMap<String, String>();
 		
-		capitals.put("United States", "Washington, DC");
-		capitals.put("Brazil", "Brazilia");
-		capitals.put("Venezuela", "Caracas");
-		capitals.put("Canada", "Ottawa");
+		employees.put(101, new Employee("Bryant", "Vail", "222 w fiesta Key Loop"));
+		employees.put(102, new Employee("Lewis", "Vail", "223 w fiesta Key Loop"));
+		employees.put(103, new Employee("John", "Vail", "224 w fiesta Key Loop"));
+		employees.put(104, new Employee("Shirley", "Vail", "225 w fiesta Key Loop"));
 		
-		System.out.println("Map = "+capitals);
+		System.out.println("Map = "+employees);
 		
-		String capital = (String)capitals.get("Brazil");
+		String capital = (String)employees.get("Brazil");
 		System.out.println("Capital of Brazil = "+capital);
 		
 		
 		
 
+	}
+	
+	class Employee{
+		
+public Employee(String firstName, String lastName, String address) {
+			
+			this.id = id;
+			this.firstName = firstName;
+			this.lastName = lastName;
+			this.address = address;
+			
+		}
+		
+		public Employee(int id, String firstName, String lastName, String address) {
+			
+			this.id = id;
+			this.firstName = firstName;
+			this.lastName = lastName;
+			this.address = address;
+			
+		}
+		
+		public int id;
+		public String firstName;
+		public String lastName;
+		public String address;
+	
 	}
 
 }
