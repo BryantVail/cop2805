e5d58821d15acfc83549a2f245c70d235b5288c8
Bryant Vail <Bryanttv@outlook.com>
2018-10-02 20:58:03 -0400

    push project week05Fibs
    fibonacci only

---
+++ week05/EuclidGcd.java
@@ -0,0 +1,31 @@
+package week05Project;
+
+/**
+ * 
+ * @author bvail
+ * 
+ * 
+ */
+
+public class EuclidGcd {
+
+	
+	//constructor
+	public EuclidGcd() {
+		//initialize
+		
+		
+	}
+	
+	private long getEuclidGcdBreak(long m, long n) {
+		
+		
+		
+	}
+	
+	public long start(long m, long n) {
+		
+	}
+	
+	
+}
