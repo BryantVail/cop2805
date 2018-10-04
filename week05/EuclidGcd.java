bbbd684ee4b355e0ef9b7d5e23d63d8a1cbb4287
Bryant Vail <Bryanttv@outlook.com>
2018-10-03 21:59:59 -0400

    euclid on

--- week05/EuclidGcd.java
+++ week05/EuclidGcd.java
@@ -19,12 +19,23 @@ public class EuclidGcd {
 	
 	private long getEuclidGcdBreak(long m, long n) {
 		
-		
-		
+		int gcd = 1;
+
+		if(m % n == 0){
+			return n;
+		}
+
+		for(int k = n /2; k >= 1; k--){
+			if(m % k == 0 && n % k ==0){
+				gcd = k;
+				break;
+			}
+		}
+		return gcd;
 	}
 	
 	public long start(long m, long n) {
-		
+		return getEuclidGcdBreak(m, n);
 	}
 	
 	
