129b284e605880f897275a09736c8d4e9f59d4ea
bxvail <BryantVail@gmail.com>
2018-10-09 19:11:26 -0400

    class files added

---
+++ week06/src/week06Practice/MergeSort.java
@@ -0,0 +1,15 @@
+package week06Practice;
+public class MergeSort {
+
+    private void merge(int[] array, int start, int middle, int end) {
+
+    }
+
+    private void mergeSort(int[] array, int start, int end) {
+
+    }
+
+    public void mergeSort(int[] array) {
+
+    }
+}
