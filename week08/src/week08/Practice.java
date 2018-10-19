85ce354fd450b21a5b9f87cc89e5148f34d6af0e
Bryant Vail <Bryanttv@outlook.com>
2018-10-18 20:42:50 -0400

    update/init for week08 project

--- week08/src/week08/Practice.java
+++ week08/src/week08/Practice.java
@@ -1,5 +1,43 @@
 package week08;
 
 public class Practice {
+	
+	public static void main(String[] args) {
+		
+		
+		/**
+		 * Search trees are not 'complete' trees. 
+		 * every sub-node on the left is less than the parent, 
+		 * 	all to the right are more
+		 */
+	}
+	
+	
 
 }
+
+class Node{
+	public Object obj;
+	public Node left;
+	public Node right;
+	
+	
+}
+
+class BinarySearchTree{
+	private Node root = null;
+	
+	public 
+	
+	
+	
+	
+	
+	
+}
+
+
+
+
+
+
