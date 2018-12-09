4a017f80508566f31bb925e541f5b6ff226adfea
bxvail <BryantVail@gmail.com>
2018-12-08 23:21:01 -0500

    update

--- week13/src/week13/Application.java
+++ week13/src/week13/Application.java
@@ -1,9 +1,23 @@
 package week13;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Application {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws SQLException, ClassNotFoundException {
 		
+		//load JDBC drover
+		Class.forName("com.mysql.jdbc.Driver");
+		System.out.println("Driver Loaded");
+		
+		//connect to database
+		Connection connection = new DriverManager.getConnection(
+		
+				"jdbc:mysql://localhost/eatSleepPrint", "root", "sqlpass"
+		);
+		System.out.println("Database connected");
 
 	}
 
